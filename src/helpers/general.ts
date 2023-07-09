f85945056f747464e11254a577495c6b62ababd1
Alexander Sudarev <sudarevae@mos-team.ru>
2023-07-09 19:31:04 +0300

    fix: merge function

--- src/helpers/general.ts
+++ src/helpers/general.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
+/* eslint-disable no-param-reassign */
 export const trim = (value: string, symbols?: string) => {
   const valueWithoutSpaces = value.replace(/\s/g, '');
 
@@ -10,3 +10,28 @@ export const trim = (value: string, symbols?: string) => {
   }
   return valueWithoutSpaces;
 };
+
+type Indexed<T = unknown> = {
+  [key in string]: T;
+};
+
+const isObject = (value: Indexed) => typeof value === 'object' && value instanceof Object;
+
+const copyObject = (source: Indexed, destination: Indexed = {}) => {
+  // eslint-disable-next-line no-restricted-syntax, guard-for-in
+  for (const key in source) {
+    if (isObject(source[key] as Indexed)) {
+      if (destination[key]) {
+        destination[key] = copyObject(destination[key] as Indexed, source[key] as Indexed);
+      } else {
+        destination[key] = copyObject(source[key] as Indexed);
+      }
+    }
+    destination[key] = source[key];
+  }
+  return destination;
+};
+
+export function merge(lhs: Indexed, rhs: Indexed): Indexed {
+  return copyObject(rhs, lhs);
+}
